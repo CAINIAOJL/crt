18822e319655606b6ae29960dbb53b60025e6957
jianglei <1625602465@qq.com>
2024-07-18 20:51:22 +0800

    malloc

--- malloc.c
+++ malloc.c
@@ -68,9 +68,72 @@ void* malloc(unsigned size) {
             next->prev = header;
             next->next = header->next;
             next->type = HEAP_BLOCK_USED;
-            
+            next->size = header - (size - HEADER_SIZE);
+            header->next = next;
+            header->size = size + HEADER_SIZE;
+            header->type = HEAP_BLOCK_USED;
+            return ADDR_ADD(header,HEADER_SIZE);
         }
         header = header->next;
     }
+    return NULL;
+}
 
+#ifdef WIN32
+//linux brk system call
+static int brk(void* end_data_segment) {
+    //brk system call number : 45
+    asm("movl $45, %%eax    \n\t"
+        "movl %1, %%ebx     \n\t"
+        "int $0x80          \n\t"
+        "movl %%eax, $0     \n\t"
+        : "=r"(ret) : "m"(end_data_segment) );
+}
+#endif
+
+#ifdef WIN32
+#include<Windows.h>
+#endif
+int_min_crt_heap_init() {
+    void* base = NULL;
+    heap_header* header = NULL;
+    unsigned size = 1024 * 1024 * 32;//32MBheap
+//windows下
+#ifdef WIN32
+//windos
+//对堆进行空间增加
+/*
+LPVOID VirtualAlloc(
+  LPVOID lpAddress,
+  DWORD dwSize,
+  DWORD flAllocationType,
+  DWORD flProtect
+);
+flAllocationType:
+  MEM_COMMIT：在内存或磁盘上的分页文件中为指定的页面区域分配物理存储。尝试提交已提交的页面不会导致函数失败。这意味着可以提交一系列已提交或取消提交的页面，而无需担心失败。
+  MEM_RESERVE：保留进程的虚拟地址空间范围，而不分配物理存储。
+ 保留范围不能由任何其他分配操作（如 malloc 和 LocalAlloc 函数）使用，直到它被释放。保留页可以在对 VirtualAlloc 函数的后续调用中提交。
+  MEM_RESET：不支持
+  MEM_TOP_DOWN：在尽可能高的地址分配内存。此标志在 Windows Mobile 中被忽略。  
+*/
+    base = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if(base == NULL) {
+        return 0;
+    }
+#else
+//linux
+//对堆进行空间增加
+    base = (void*)brk(0);
+    void* end = ADDR_ADD(base, size);
+    end = (void*)brk(end);
+    if(!end) {
+        return 0;
+    }
+#endif
+
+    list_head = (heap_header*)base;
+    list_head->type = HEAP_BLOCK_FREE;
+    list_head->size = size;
+    list_head->prev = NULL;
+    list_head->next = NULL;
 }
